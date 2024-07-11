be18b55f7e692b6616584ee984f6c1488ae9e03c
gasdar <gottafindshape@gmail.com>
2024-07-11 16:10:57 -0400

    Inicio del Proyecto de Login Respaldado

---
+++ src/main/java/com/rolan/login/Login.java
@@ -0,0 +1,16 @@
+package com.rolan.login;
+
+public class Login {
+
+    /*
+    Lo primero que sea a de realizar es la estructuración del proyecto,
+    o la división de capas, que busca implementar algún patrón de diseño
+    o arquitectura como el MVC (Modelo, Vista, Controlador). En este caso,
+    se utiliza el MC (Modelo de Capas), muy parecido al MVC
+    */
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
